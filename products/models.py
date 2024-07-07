79c59ce9a2b034ae2b52a91ddcc5ff9a0fc391ff
Avni Goel <avnigoel98@gmail.com>
2024-07-07 21:15:11 +0530

    added size variant for products

--- products/models.py
+++ products/models.py
@@ -22,9 +22,15 @@ class ColorVariant(BaseModel):
     color_name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
 
+    def __str__(self) -> str:
+        return self.color_name
+
 class SizeVariant(BaseModel):
     size_name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.size_name
 
 
 class Product(BaseModel):
@@ -33,8 +39,8 @@ class Product(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     price = models.IntegerField()
     product_description = models.TextField()
-    color_variant = models.ManyToManyField(ColorVariant)
-    size_variant = models.ManyToManyField(SizeVariant)
+    color_variant = models.ManyToManyField(ColorVariant, blank=True)
+    size_variant = models.ManyToManyField(SizeVariant, blank = True)
 
     def save(self, *args , **kwargs):
         self.slug = slugify(self.product_name)
@@ -42,6 +48,9 @@ class Product(BaseModel):
 
     def __str__(self) -> str:
         return self.product_name
+    
+    def get_product_price_by_size(self, size):
+        return self.price + SizeVariant.objects.get(size_name = size).price
 
 
 
--- products/views.py
+++ products/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
-from products.models import *
 # Create your views here.
-
+from products.models import *
 
 def get_product(request, slug):
     try:
         product = Product.objects.get(slug = slug)
-        return render(request, 'product/products.html', context= {'product' : product}) 
+        context= {'product' : product}
+        if request.GET.get('size'):
+            size = request.GET.get('size')
+            #print(size)
+            price = product.get_product_price_by_size(size)
+            context['selected_size']  =  size
+            context['updated_price']  =  price
+
+        return render(request, 'product/products.html', context= context) 
     except Exception as e:
         print(e)
